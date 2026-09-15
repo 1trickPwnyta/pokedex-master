50f686d51737cd1170f76f3ec50ac4ab5395c741
1trickPwnyta <1trickPwnyta@kangaroostandard.com>
2026-09-14 19:18:17 -0700

    Add generation 2 pokemon

--- static/main.js
+++ static/main.js
@@ -13,14 +13,17 @@ const ANSWER = document.getElementById("answer");
 const ANSWER_BOX = document.getElementById("answer-box");
 const ANSWER_FIELD = document.getElementById("answer-field");
 const OPTIONS = document.getElementById("options");
+const SETTINGS = document.getElementById("settings");
 const MESSAGE_BACKGROUND = document.getElementById("message-background");
 
 const OPTION_START = { text: "Start", action: onOptionStart };
 const OPTION_GIVEUP = { text: "Give up", action: onOptionGiveUp };
 const OPTION_RESTART = { text: "Restart", action: onOptionRestart };
 const OPTION_HELP = { image: "./help-box.png", action: onOptionHelp };
+const OPTION_SETTINGS = { image: "./cog-box.png", action: onOptionSettings };
 
 const SOUND_CLICK = new Audio("./click.wav");
+const SOUND_REJECT = new Audio("./reject.wav");
 const SOUND_GIVEUP = new Audio("./giveup.wav");
 const SOUND_WIN = new Audio("./win.wav");
 
@@ -40,7 +43,15 @@ let data = await dataFetch.json();
 
 const options = [];
 addOption(OPTION_START);
-addOption(OPTION_HELP);
+
+const dialogStack = [];
+
+addSetting(OPTION_HELP);
+addSetting(OPTION_SETTINGS);
+
+if (!localStorage.generations || !JSON.parse(localStorage.generations).length) {
+	localStorage.generations = JSON.stringify([ data.generations[0].name ]);
+}
 
 function flushStyle(element) {
 	void element.offsetHeight;
@@ -50,6 +61,11 @@ function cacheImage(url) {
 	const image = new Image();
 	image.fetchPriority = "high";
 	image.src = url;
+}
+
+function jitter(element) {
+	element.classList.add("jitter");
+	setTimeout(() => element.classList.remove("jitter"), 250);
 }
 
 function getSpriteUrl(number, pokemon) {
@@ -82,14 +98,14 @@ function makeButton(text, image, action) {
 }
 
 function addOption(opt) {
-	let option = makeButton(opt.text, opt.image, opt.action);
+	const option = makeButton(opt.text, opt.image, opt.action);
 	opt.option = option;
 	OPTIONS.appendChild(option);
 	options.push(opt);
 }
 
 function removeOption(opt) {
-	let option = options.find(o => o.text == opt.text);
+	const option = options.find(o => o.text == opt.text);
 	if (option) {
 		OPTIONS.removeChild(option.option);
 		options.splice(options.indexOf(option), 1);
@@ -100,6 +116,11 @@ function removeAllOptions() {
 	while (options.length) {
 		removeOption(options[0]);
 	}
+}
+
+function addSetting(set) {
+	const setting = makeButton(set.text, set.image, set.action);
+	SETTINGS.appendChild(setting);
 }
 
 function simplifyName(name) {
@@ -134,13 +155,17 @@ function onOptionHelp() {
 	showHelp();
 }
 
+function onOptionSettings() {
+	showSettings();
+}
+
 function updateTimer() {
 	const milliseconds = new Date() - startTime;
 	TIMER.innerText = dayjs.duration(milliseconds).format("mm:ss");
 }
 
 function updateProgress() {
-	const progress = `${level}<br />/ ${pokemon.length}`;
+	const progress = `${level}<br />/ ${board.length}`;
 	PROGRESS.innerHTML = progress;
 }
 
@@ -182,7 +207,6 @@ function clearCollection() {
 function startGame() {
 	removeAllOptions();
 	addOption(OPTION_GIVEUP);
-	addOption(OPTION_HELP);
 	buildBoard();
 	level = -1;
 	showAnswerBox();
@@ -196,7 +220,6 @@ function startGame() {
 function stopGame() {
 	removeAllOptions();
 	addOption(OPTION_RESTART);
-	addOption(OPTION_HELP);
 	hideAnswerBox();
 	if (timerUpdate) {
 		clearInterval(timerUpdate);
@@ -207,11 +230,15 @@ function stopGame() {
 function win() {
 	SOUND_WIN.play();
 	stopGame();
-	setQuestion(`Good job!<br />You named ${pokemon.length} Pokémon in ${TIMER.innerText}.`);
+	setQuestion(`Good job!<br />You named ${board.length} Pokémon in ${TIMER.innerText}.`);
 }
 
 function buildBoard() {
-	board = Array.from({ length: pokemon.length }, (_, i) => i + 1);
+	const generations = JSON.parse(localStorage.generations);
+	board = [];
+	for (const generation of data.generations.filter(g => generations.includes(g.name))) {
+		board = [ ...board, ...Array.from({ length: generation.range[1] - generation.range[0] + 1 }, (_, i) =>  generation.range[0] + i) ];
+	}
 	board = arrayShuffle(board);
 }
 
@@ -273,8 +300,10 @@ function blinkAnswerField() {
 }
 
 function doDialog(options) {
-	MESSAGE_BACKGROUND.innerHTML = "";
 	MESSAGE_BACKGROUND.style.visibility = "visible";
+	if (dialogStack.length) {
+		dialogStack.at(-1).style.display = "none";
+	}
 	
 	let messageBox = document.createElement("div");
 	messageBox.className = "message-box card light";
@@ -282,13 +311,23 @@ function doDialog(options) {
 	let messageArea = document.createElement("div");
 	messageArea.className = "message-area";
 	if (options.title) {
-		messageArea.innerHTML = `<div class="message-title">${options.title}</div>`;
+		const titleElement = document.createElement("div");
+		titleElement.className = "message-title";
+		if (options.titleJustification) {
+			titleElement.style.textAlign = options.titleJustification;
+		}
+		titleElement.innerText = options.title;
+		messageArea.appendChild(titleElement);
 	}
 	if (options.subtitle) {
 		messageArea.innerHTML += `<div class="message-subtitle">${options.subtitle}</div>`;
 	}
 	if (options.message) {
 		messageArea.innerHTML += `<div class="message-text">${options.message}</div>`;
+	}
+	if (options.element) {
+		options.element.style.width = "100%";
+		messageArea.appendChild(options.element);
 	}
 	messageBox.appendChild(messageArea);
 	
@@ -306,17 +345,79 @@ function doDialog(options) {
 	messageBox.appendChild(messageClose);
 	
 	MESSAGE_BACKGROUND.appendChild(messageBox);
+	dialogStack.push(messageBox);
 }
 
 function confirm(message, onYes, onNo, onCancel) {
-	let yesButton = makeButton("Yes", null, () => closeMessage(onYes));
-	let noButton = makeButton("No", null, () => closeMessage(onNo));
+	const yesButton = makeButton("Yes", null, () => closeMessage(onYes));
+	const noButton = makeButton("No", null, () => closeMessage(onNo));
 	doDialog({ title: message, buttons: [ yesButton, noButton ], onCancel: onCancel });
 }
 
+function choices(title, choices, onSubmit, onCancel, minSelections) {
+	const checkboxes = [];
+	
+	const element = document.createElement("div");
+	for (const choice of choices) {
+		const choiceElement = document.createElement("div");
+		choiceElement.className = "dialog-choice";
+		
+		const click = () => {
+			if (minSelections
+				&& choiceCheckbox.checked
+				&& checkboxes.filter(c => c.checked).length <= minSelections) {
+				SOUND_REJECT.play();
+				jitter(choiceCheckbox);
+			} else {
+				SOUND_CLICK.play();
+				choiceCheckbox.checked = !choiceCheckbox.checked;
+			}
+		};
+		
+		const choiceCheckbox = document.createElement("input");
+		choiceCheckbox.type = "checkbox";
+		choiceCheckbox.value = choice.value;
+		choiceCheckbox.id = `choices-${choice.value}`;
+		choiceCheckbox.onclick = e => {
+			choiceCheckbox.checked = !choiceCheckbox.checked;
+			click();
+		};
+		choiceCheckbox.checked = choice.selected;
+		choiceElement.appendChild(choiceCheckbox);
+		checkboxes.push(choiceCheckbox);
+		
+		const choiceLabel = document.createElement("label");
+		choiceLabel.for = choiceCheckbox.id;
+		choiceLabel.innerText = choice.value;
+		choiceLabel.onclick = click;
+		choiceElement.appendChild(choiceLabel);
+		
+		element.appendChild(choiceElement);
+	}
+	
+	const submitButton = makeButton("Save", null, () => closeMessage(() => {
+		onSubmit(checkboxes.filter(c => c.checked).map(c => c.value));
+	}));
+	const cancelButton = makeButton("Cancel", null, () => closeMessage(onCancel));
+	
+	doDialog({
+		title: title,
+		element: element,
+		buttons: [ submitButton, cancelButton ],
+		onCancel: onCancel
+	});
+}
+
 function closeMessage(callback = () => {}) {
-	MESSAGE_BACKGROUND.style.visibility = "hidden";
-	callback();
+	MESSAGE_BACKGROUND.removeChild(dialogStack.pop());
+	if (dialogStack.length) {
+		dialogStack.at(-1).style.display = "flex";
+	} else {
+		MESSAGE_BACKGROUND.style.visibility = "hidden";
+	}
+	if (callback) {
+		callback();
+	}
 }
 
 function showHelp() {
@@ -325,6 +426,26 @@ function showHelp() {
 		subtitle: "by 1trickPwnyta",
 		message: `<p>So you can name all the Pokémon in dex order from memory and you think you're real smart, huh? But can you name them all <em>by dex number</em> in random order?</p>
 <p>The path to Pokédex mastery is long and difficult. Do you have what it takes?</p>`
+	});
+}
+
+function showSettings() {
+	const element = document.createElement("div");
+	
+	const generationsButton = makeButton("Generations", null, () => {
+		const selected = JSON.parse(localStorage.generations);
+		choices("Generations", data.generations.map(g => {
+			return { value: g.name, selected: selected.includes(g.name) };
+		}), selections => {
+			localStorage.generations = JSON.stringify(selections);
+		}, null, 1);
+	});
+	element.appendChild(generationsButton);
+	
+	doDialog({
+		title: "Options",
+		titleJustification: "center",
+		element: element
 	});
 }
 
