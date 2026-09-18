59235e3badfea384c829ed48217e4d552d36b8ad
1trickPwnyta <1trickPwnyta@kangaroostandard.com>
2026-09-17 23:50:22 -0700

    Fix image caching

--- src/main.js
+++ src/main.js
@@ -56,9 +56,11 @@ function flushStyle(element) {
 }
 
 async function cacheImage(url) {
+	const response = await fetch(url);
+	const blob = await response.blob();
 	const image = new Image();
 	image.fetchPriority = "high";
-	image.src = url;
+	image.src = URL.createObjectURL(blob);
 	await image.decode();
 }
 
